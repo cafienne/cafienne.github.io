58934e6319b71bc89646dda348927be18eed0633
Paul Verbeek-Mast <paulvm@Pauls-MacBook-Pro.local>
2017-09-12 14:48:15 +0200

    reverse all paths to have double the fun 😄

--- script.js
+++ script.js
@@ -172,15 +172,17 @@ function createFullPath(path) {
   return allCoords;
 }
 
-const paths = [
-  [[-3, 3], [0, 3], [1, 2], [2, 1], [2, -2], [1, -2]],
+let paths = [
   [[0, 0], [0, 2], [-4, 2], [-4, 0], [-3, -1]],
+  [[-3, 3], [0, 3], [2, 1], [2, -2], [1, -2]],
   [[3, -2], [3, -3], [0, -3], [-3, 0], [-3, 2]],
   [[0, -5], [-2, -3], [-1, -3], [3, -3], [3, -1]],
   [[5, -5], [3, -5], [-2, 0], [-2, 2]],
   [[0, -4], [-2, -2], [2, -2], [2, 1]],
   [[-4, 3], [-2, 3], [-2, 0], [2, -4]]
 ].map(createFullPath);
+
+paths = paths.concat(paths.map(path => Array.from(path).reverse()));
 
 function getDots() {
   const path = paths[Math.floor(Math.random() * paths.length)];
